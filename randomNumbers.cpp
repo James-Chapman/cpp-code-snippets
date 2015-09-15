be8e43c51d773d3338985d6eb639187df0594529
James Chapman <james@uplinkzero.com>
2015-09-15 15:44:56 +0100

    Create randomNumbers.cpp

---
+++ randomNumbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <random>
+
+int main()
+{
+    std::random_device rd;
+    std::mt19937 urng(rd());
+    
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << urng() << std::endl;
+    }
+
+    return 0;
+}
